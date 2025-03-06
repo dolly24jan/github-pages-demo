a8228fd210c866bb77e1f2343e1f15b6e1d57074
dolly24jan <74894874+dolly24jan@users.noreply.github.com>
2025-03-06 18:40:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Chandan Tiwari!
+          Hello and Welcome to CI/CD Pipelines !
         </a>
       </header>
     </div>
